29d108ca986d713cbce94ad15428c62f988cadce
ihyunbeom <ihyunbeom@gmail.com>
2017-05-26 19:39:51 +0900

    로그인, 회원가입

---
+++ HatTrick/app/src/main/java/org/hat/hattrick/AppConfig.java
@@ -0,0 +1,13 @@
+package org.hat.hattrick;
+
+/**
+ * Created by DIE on 2017-05-26.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.200.112/hattrick_db/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.200.112/hattrick_db/register.php";
+}
