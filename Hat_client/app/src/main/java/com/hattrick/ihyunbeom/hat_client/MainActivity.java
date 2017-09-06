a5ed9b8d88c3f40522a1ebf9bcb63d87b48d134c
ihyunbeom <ihyunbeom@gmail.com>
2017-09-07 03:37:31 +0900

    1.7.2 버전 apk 빌드

--- Hat_client/app/src/main/java/com/hattrick/ihyunbeom/hat_client/MainActivity.java
+++ Hat_client/app/src/main/java/com/hattrick/ihyunbeom/hat_client/MainActivity.java
@@ -59,14 +59,14 @@ public class MainActivity extends AppCompatActivity
                 "lose integer);");
 
         if (isFirstTime()) {
-            //sqLiteHelper.queryDate("insert into position(fw, mf, df, gk, total) values(0,0,0,0,0);");
-            sqLiteHelper.queryDate("insert into position(fw, mf, df, gk, total) values(3,10,6,3,22);");
+            sqLiteHelper.queryDate("insert into position(fw, mf, df, gk, total) values(0,0,0,0,0);");
+            //sqLiteHelper.queryDate("insert into position(fw, mf, df, gk, total) values(3,10,6,3,22);");
             sqLiteHelper.queryDate("insert into score(games, goals, lost, win, draw, lose) values(0,0,0,0,0,0);");
             sqLiteHelper.queryDate("insert into team_info(team, manager, created) values('팀명을 설정하세요','매니저 이름을 설정하세요','1993.12.24');");
 
             // 선수등록 테스트
 
-
+            /*
             sqLiteHelper.queryDate("insert into player(name, position, goal, outing) values('이동국','FW',0,0);");
             sqLiteHelper.queryDate("insert into player(name, position, goal, outing) values('김신욱','FW',0,0);");
             sqLiteHelper.queryDate("insert into player(name, position, goal, outing) values('황희찬','FW',0,0);");
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity
             sqLiteHelper.queryDate("insert into player(name, position, goal, outing) values('김승규','GK',0,0);");
             sqLiteHelper.queryDate("insert into player(name, position, goal, outing) values('김진현','GK',0,0);");
             sqLiteHelper.queryDate("insert into player(name, position, goal, outing) values('김영권','GK',0,0);");
-
+            */
 
 
 
